72f17c827a9f2a5d92c510ef240f6d7b1fbd8989
seba230903 <lopezdominguez.seba@gmail.com>
2022-09-09 17:58:55 -0700

    task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end='')
+        if i == 8 and j == 9:
+            continue
+        print(", ", end='')
+print('')
